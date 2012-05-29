52ada6be5dd70cdaf2072c7b068c284496c60687
Peter G <peter@fol.io>
2012-05-29 11:19:39 -0700

    Added disallowed_matchers

--- lib/safety_mailer/safety_mailer.rb
+++ lib/safety_mailer/safety_mailer.rb
@@ -1,23 +1,39 @@
 module SafetyMailer
   class Carrier
-    attr_accessor :matchers, :settings
+    attr_accessor :allowed_matchers, :disallowed_matchers, :settings
+    
     def initialize(params = {})
-      self.matchers = params[:allowed_matchers] || []
+      self.allowed_matchers = params[:allowed_matchers] || []
+      self.disallowed_matchers = params[:disallowed_matchers] || []
       self.settings = params[:delivery_method_settings] || {}
       delivery_method = params[:delivery_method] || :smtp
       @delivery_method = Mail::Configuration.instance.lookup_delivery_method(delivery_method).new(settings)
     end
+    
     def log(msg)
       Rails.logger.warn(msg) if defined?(Rails)
     end
-    def deliver!(mail)
-      mail.to = mail.to.reject do |recipient|
-        if matchers.any?{ |m| recipient =~ m }
+    
+    def filter_recipient(recipient)
+      if allowed_matchers.empty?
+        if disallowed_matchers.any?{ |m| recipient =~ m } 
+          log "*** safety_mailer suppressing mail to #{recipient}"
+          return true
+        end
+        return false
+      else
+        if allowed_matchers.any?{ |m| recipient =~ m }
           false
         else
           log "*** safety_mailer suppressing mail to #{recipient}"
           true
         end
+      end
+    end
+    
+    def deliver!(mail)
+      mail.to = mail.to.reject do |recipient|
+        filter_recipient(recipient)
       end
       if mail.to.nil? || mail.to.empty?
         log "*** safety_mailer - no recipients left ... suppressing delivery altogether"
@@ -26,5 +42,6 @@ module SafetyMailer
         @delivery_method.deliver!(mail)
       end
     end
+    
   end
 end
